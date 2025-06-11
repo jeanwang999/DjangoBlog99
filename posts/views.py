8b42f2a93714e747a912c6bb7f431b30cbc67cfd
jeanwang999 <weizi.wang75@gmail.com>
2025-06-11 12:00:42 +0800

    20250611 view & urls

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from posts.models import Post
+
 
 # Create your views here.
+
+# def定義想要新增的頁面的名稱 再去urls.py 新增path
+def index(request):
+    article_records = Post.objects.all()
+    article_list = list()
+    for count, article in enumerate(article_records):
+        article_list.append("#{}: {}<br><hr>".format(str(count), str(article)))
+        article_list.append("<small>{}</small><br> slug:{}<br><hr> ".format(article.content,article.slug))
+    return HttpResponse(article_list)
+
+# def定義想要新增的頁面的名稱 再去urls.py 新增path
+
+def about(request):
+    
