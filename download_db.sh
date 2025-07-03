5f926d5e5768625bd3fc34df4c8c9ba0ca3f505f
jeanwang999 <weizi.wang75@gmail.com>
2025-07-03 10:16:42 +0800

    20250703-3

--- download_db.sh
+++ download_db.sh
@@ -1,2 +1,2 @@
 rm -rf db.sqlite3
-download_dbwget --no-check-certificate "https://docs.google.com/uc?export=download&id=1TqXmqW0II0G8nFsni3cwLtEpQFIS4z5H" -O db.sqlite3
+wget --no-check-certificate "https://docs.google.com/uc?export=download&id=1TqXmqW0II0G8nFsni3cwLtEpQFIS4z5H" -O db.sqlite3
