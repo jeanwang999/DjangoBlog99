9ddd7b956b34a1a66948914119e431968a1d577e
jeanwang999 <weizi.wang75@gamil.com>
2025-07-03 09:48:30 +0800

    20250703 db file setting

---
+++ download_db.bat
@@ -0,0 +1,1 @@
+wget --no-check-certificate "https://docs.google.com/uc?export=download&id=1TqXmqW0II0G8nFsni3cwLtEpQFIS4z5H" -O db.sqlite3
---
+++ download_db.sh
@@ -0,0 +1,2 @@
+rm -rrm -rf db.sqlite3
+download_dbwget --no-check-certificate "https://docs.google.com/uc?export=download&id=1TqXmqW0II0G8nFsni3cwLtEpQFIS4z5H" -O db.sqlite3
