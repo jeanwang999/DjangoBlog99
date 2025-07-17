94fc1dbe6dc2f527217a428606fd2769396dc264
jeanwang999 <weizi.wang75@gmail.com>
2025-07-17 11:43:37 +0800

    0717 build mySQL

--- DjangoBlog99/settings.py
+++ DjangoBlog99/settings.py
@@ -80,6 +80,8 @@ WSGI_APPLICATION = "DjangoBlog99.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+# Sqlite
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
@@ -87,6 +89,23 @@ DATABASES = {
     }
 }
 
+"""
+# mySQL
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "django_db",
+        "USER": "django_db",
+        "PASSWORD": "JMFH0Eb8!ISaC@[)",
+        "HOST": "localhost",
+        "PORT": "3306",
+        "OPTIONS": {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
+    }
+}
+
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
